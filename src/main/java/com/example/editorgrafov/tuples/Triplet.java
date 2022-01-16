b7fd899ddc35dbe29d7ed91ac5fa1cc72dfef7a4
Loso9 <92187237+Loso9@users.noreply.github.com>
2022-01-16 01:38:20 +0100

    uploading tuples used in Editor

---
+++ src/main/java/com/example/editorgrafov/tuples/Triplet.java
@@ -0,0 +1,52 @@
+package com.example.editorgrafov;
+
+import java.util.Objects;
+
+public class Triplet<P, Q, R> {
+    private P first;
+    private Q second;
+    private R third;
+
+    public Triplet(P first, Q second, R third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+
+    public P getFirst() {
+        return first;
+    }
+
+    public Q getSecond() {
+        return second;
+    }
+
+    public R getThird() {
+        return third;
+    }
+
+    public void setFirst(P first) {
+        this.first = first;
+    }
+
+    public void setSecond(Q second) {
+        this.second = second;
+    }
+
+    public void setThird(R third) {
+        this.third = third;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Triplet<?, ?, ?> triplet = (Triplet<?, ?, ?>) o;
+        return Objects.equals(first, triplet.first) && Objects.equals(second, triplet.second) && Objects.equals(third, triplet.third);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second, third);
+    }
+}
