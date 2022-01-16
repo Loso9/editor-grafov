504a46bdb67cde8a109fa60324ba3b3336619750
Loso9 <92187237+Loso9@users.noreply.github.com>
2022-01-16 01:37:38 +0100

    uploading enums used in Editor

---
+++ src/main/java/com/example/editorgrafov/enums/Action.java
@@ -0,0 +1,8 @@
+package com.example.editorgrafov.enums;
+
+/**
+ * Enum for options in File tab in menu
+ */
+public enum Action {
+    NEW, OPEN, EXIT, SAVE, SAVEAS;
+}
---
+++ src/main/java/com/example/editorgrafov/enums/Mode.java
@@ -0,0 +1,8 @@
+package com.example.editorgrafov.enums;
+
+/**
+ * Enum for options in Mode tab of menu
+ */
+public enum Mode {
+    INSERT, DELETE, ADDEDGE;
+}
